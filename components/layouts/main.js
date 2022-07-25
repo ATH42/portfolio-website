3653004a5345f655dab955db68bd4637da4598e3
ATH <hoffmann@sciendis.de>
2022-07-25 13:26:00 +0200

    add animation with three; format;

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,17 +1,23 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
+import NoSsr from '../no-ssr'
 import Navbar from '../navbar.js'
+import VoxelAnimation from '../voxel-animation'
+
 const Main = ({ children, router }) => {
   return (
-    <Box as="main">
+    <Box as='main'>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>Alexander Theofilos Hoffmann - Homepage</title>
       </Head>
 
       <Navbar path={router.asPath} />
 
-      <Container maxW="container.md" pt={{ base: 40, md: 32 }}>
+      <Container maxW='container.md'>
+        <NoSsr>
+          <VoxelAnimation />
+        </NoSsr>
         {children}
       </Container>
     </Box>
