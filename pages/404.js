e0555ba40c9a3618b859b3495ff887e90d75bc11
ATH <alex.t.hoffmann@icloud.com>
2022-10-02 21:22:03 +0200

    add ts particles;

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,20 @@
 import Nextlink from 'next/link'
 import { Box, Heading, Text, Container, Button } from '@chakra-ui/react'
-
+import Particle from '../components/particle'
 const NotFound = () => {
   return (
-    <Container>
-      <Heading as="h1">Not Found</Heading>
-      <Text>The Page you&apos;re looking for was not found</Text>
-      <Box my={6} align="center">
-        <Nextlink href="/">
-          <Button colorScheme="blue">Return to Home</Button>
-        </Nextlink>
-      </Box>
-    </Container>
+    <>
+      <Container>
+        <Heading as="h1">404</Heading>
+        <Text>The Page you&apos;re looking for was not found</Text>
+        <Particle />
+        <Box my={6} align="center">
+          <Nextlink href="/">
+            <Button colorScheme="blue">Return to Home</Button>
+          </Nextlink>
+        </Box>
+      </Container>
+    </>
   )
 }
 
